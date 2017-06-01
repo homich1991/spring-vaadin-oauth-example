5d82d072caef708681c9f2a2bfc6ca345fb991d9
homich1991 <homich1991@gmail.com>
2017-06-01 14:32:38 +0400

    class renaming

--- auth-impl/src/main/java/com/homich/AuthApplication.java
+++ auth-impl/src/main/java/com/homich/AuthApplication.java
@@ -19,10 +19,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SocialApplication {
+public class AuthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SocialApplication.class, args);
+        SpringApplication.run(AuthApplication.class, args);
     }
 
 }
